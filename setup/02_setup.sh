d964a070429876ba84ea586aa08a603055e8cb9c
Olivier Bourdon <obourdon@opennext.io>
2018-01-17 15:35:36 +0100

    Add more genericity, fix some errors

--- setup/02_setup.sh
+++ setup/02_setup.sh
@@ -17,7 +17,7 @@ sudo pip install -U pip
 curl -sSL https://get.docker.io | bash
 
 # Allow the local user to run and control docker
-sudo usermod -aG docker hoge
+sudo usermod -aG docker $(id -nu)
 
 # Set up Kolla specific configurations for docker
 sudo mkdir -p /etc/systemd/system/docker.service.d
@@ -37,27 +37,33 @@ sudo systemctl enable docker
 sudo yum install -y python-docker-py
 
 # If it's running, stop and disable the libvirt service
+set +e
 sudo systemctl stop libvirtd.service
 sudo systemctl disable libvirtd.service
+set -e
 
 # update pip and everything else
 sudo pip install --upgrade pip virtualenv setuputils
 
 # Prepare the virtualenv
-mkdir kolla
+mkdir -p kolla
 cd kolla
 virtualenv .kolla
 source .kolla/bin/activate
 pip install tox ansible
 
 #Download and install kolla from source in a virtual environment
-git clone https://github.com/openstack/kolla
+if [ ! -d kolla ]; then
+	git clone https://github.com/openstack/kolla
+fi
 cd kolla
 pip install --upgrade
 pip install -r requirements.txt
 pip install .
 cd ..
-git clone https://github.com/openstack/kolla-ansible
+if [ ! -d kolla-ansible ]; then
+	git clone https://github.com/openstack/kolla-ansible
+fi
 cd kolla-ansible
 pip install -r requirements.txt
 pip install .
@@ -66,18 +72,16 @@ sudo cp -r kolla-ansible/etc/kolla /etc/kolla/
 cp kolla-ansible/ansible/inventory/* .
 
 # download the ironic agent
-mkdir config
-mkdir config/ironic
-curl http://tarballs.openstack.org/ironic-python-agent/coreos/files/coreos_production_pxe.vmlinuz \
+mkdir -p config/ironic
+curl -C - http://tarballs.openstack.org/ironic-python-agent/coreos/files/coreos_production_pxe.vmlinuz \
 	-o config/ironic/ironic-agent.kernel -4
-curl http://tarballs.openstack.org/ironic-python-agent/coreos/files/coreos_production_pxe_image-oem.cpio.gz \
+curl -C - http://tarballs.openstack.org/ironic-python-agent/coreos/files/coreos_production_pxe_image-oem.cpio.gz \
 	-o config/ironic/ironic-agent.initramfs -4
 
 # copy configuration
 cp kolla-ansible/ansible/inventory/* .
-cp ~/symmetrical-memory/ironic/globals.yml globals.yml
+#cp ~/symmetrical-memory/ironic/globals.yml globals.yml
 
 # Install and start the docker registry
 sudo docker run -d --name registry --restart=always -p 4000:5000 \
 	-v /registry:/var/lib/registry registry:2 
-
--- setup/03_build.sh
+++ setup/03_build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
+CMDDIR=$(readlink -f $(dirname $0))
 # as root
-cd /home/hoge/kolla
+cd $CMDDIR/kolla
 source .kolla/bin/activate
 kolla-build --registry 192.168.1.6:4000 --push -t source -T 8
